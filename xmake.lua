eebccb208fb0367ca128d4e1b6c12cd2fc899861
xiaguan <751080330@qq.com>
2023-01-07 18:53:15 +0800

    finish skiplist, add test ,build error

--- xmake.lua
+++ xmake.lua
@@ -1,12 +1,17 @@
+set_project("SuKV")
+
 add_rules("mode.debug", "mode.release")
 
 set_languages("c++latest")
 
-target("mod")
+add_requires("gtest")
+
+target("SkipList")
     set_kind("static")
     add_files("src/*.cpp", "src/*.mpp")
 
-target("test")
+target("SkipListTest")
     set_kind("binary")
-    add_files("test/*.cpp")
-    add_deps("mod")
+    add_files("test/skiplist_test.cpp")
+    add_deps("SkipList")
+    add_packages("gtest")
